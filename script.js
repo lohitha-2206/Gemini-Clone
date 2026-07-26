7ac5d7eb0f5a04fb1dbb2590624dc39e0df36430
Savvana Lohitha <lohithasavvana@gmail.com>
2026-07-26 15:56:05 +0530

    Update API key and URL for response generation

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const fileInput = promptForm.querySelector("#file-input");
 const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper");
 const themeToggle = document.querySelector("#theme-toggle-btn");
 
-const API_KEY = "AIzaSyCDNCQ3vLOhhRWhaijPzP7ua5zsPTYySsM";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+const API_KEY = "AQ.Ab8RN6KGNoW_De69kRIef6i3mVAnMtFYoTITkDeUUu9VORYAxA";
+const API_URL =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent";
 
 let typingInterval, controller;
 const chatHistory = [];
@@ -65,11 +66,16 @@ const generateResponse = async (botMsgDiv) => {
 
   try {
     const response = await fetch(API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ contents: chatHistory }),
-      signal: controller.signal,
-    });
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "X-goog-api-key": API_KEY,
+  },
+  body: JSON.stringify({
+    contents: chatHistory,
+  }),
+  signal: controller.signal,
+});
 
     const data = await response.json();
     if (!response.ok) throw new Error(data.error.message);
